2143b7084c170488de36483fd11fc04f8ee72668
Evan Moore <etmoore@gmail.com>
2014-11-13 10:07:19 -0700

    controller and view for show

--- app/controllers/status_updates_controller.rb
+++ app/controllers/status_updates_controller.rb
@@ -1,5 +1,20 @@
 class StatusUpdatesController < ApplicationController
+  before_action :get_status_update, only: [:update, :destroy, :show, :edit]
+
   def index
     @status_updates = StatusUpdate.all
   end
+
+  def show
+  end
+
+  private
+
+    def get_status_update
+      @status_update = StatusUpdate.find(params[:id])
+    end
+
+    def status_update_params
+      params.require(:status_update).permit(:user, :status, :likes)
+    end
 end
